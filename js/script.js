f23ac21430450ecb0d5b5f5f4f55361366df0aab
sia <sashin9622@gmail.com>
2024-10-07 13:22:37 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ $(function () {
     const duration = 400;
     const duration2 = 150;
 
-    // gnb
     $menu.on("mouseenter", function () {
         $(this).addClass("on");
         $header.addClass("active");
@@ -23,6 +22,34 @@ $(function () {
     $window.on("wheel", function (e) {
         e.originalEvent.wheelDelta > 0 ? $header.removeClass("hide") : $header.addClass("hide");
     });
+
+    // 메뉴 리스트 버튼
+    const $btn = $(".meun-btn > li");
+    const $btn2 = $(".all-menu-btn > li");
+
+    btnAction(0);
+    btn2Action(0);
+
+    $btn.on("click", function () {
+        $(this).siblings().removeClass("on");
+        $(this).toggleClass("on");
+    });
+
+    function btnAction(index) {
+        $btn.removeClass("on");
+        $btn.eq(index).addClass("on");
+    }
+
+    // 서브
+    $btn2.on("click", function () {
+        $(this).siblings().removeClass("on");
+        $(this).toggleClass("on");
+    });
+
+    function btn2Action(index) {
+        $btn2.removeClass("on");
+        $btn2.eq(index).addClass("on");
+    }
 });
 
 // 모바일 메뉴
